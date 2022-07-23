2d22dd83f2043729364cd2dd9533726d31e46b91
aabelyakov <aabelyakov@mail.ru>
2022-07-23 21:32:01 +0300

    q

--- cw002.py
+++ cw002.py
@@ -11,3 +11,23 @@
 # multiply(200)==25000 # 200 * 5³
 # multiply(0)==0 # 0 * 5¹
 # multiply(-3)==-15 # -3 * 5¹
+
+
+def multiply(number):
+    if number < 0:
+        # minus = True
+        n = len(str(number)) - 1
+    else:
+        n = len(str(number))
+    # endif
+    return number * pow(5, n)
+# enddef
+
+if __name__ == "__main__":
+    assert multiply(3) == 15  # 3 * 5¹
+    assert multiply(-10) == -250  # -10 * 5²
+    assert multiply(200) == 25000  # 200 * 5³
+    assert multiply(0) == 0  # 0 * 5¹
+    assert multiply(-3) == -15  # -3 * 5¹
+    assert multiply(1) == 5  # 1 * 5¹
+# endif
