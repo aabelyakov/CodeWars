53b3a96b4221bdbd25dad76200b8e88f7db5af2f
aabelyakov <aabelyakov@mail.ru>
2022-07-24 06:59:03 +0300

    q

--- cw002.py
+++ cw002.py
@@ -1,11 +1,7 @@
-# DESCRIPTION:
-# Jack really likes his number five: the trick here is that you have to
-# multiply each number by 5 raised to the number of digits of each numbers,
-# so, for example:
 # Джеку очень нравится его номер пять: хитрость здесь в том, что вы должны
 # умножить заданное число на 5, возведенное в число цифр заданного
 # числа
-#
+# Например:
 # multiply(3)==15 # 3 * 5¹
 # multiply(10)==250 # 10 * 5²
 # multiply(200)==25000 # 200 * 5³
@@ -14,13 +10,10 @@
 
 
 def multiply(number):
-    if number < 0:
-        # minus = True
-        n = len(str(number)) - 1
-    else:
-        n = len(str(number))
-    # endif
-    return number * pow(5, n)
+    n = len(str(abs(number)))
+    res = number * pow(5, n)
+    # print(res)
+    return res
 # enddef
 
 if __name__ == "__main__":
