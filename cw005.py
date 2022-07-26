b91cb43c85b12ebc22f9335d3e23725757b3284a
aabelyakov <aabelyakov@mail.ru>
2022-07-26 09:38:03 +0300

    q

--- cw005.py
+++ cw005.py
@@ -1,15 +1,11 @@
 # Дана строка чисел, разделенных пробелами.
 # Необходимо вернуть строку с наибольшим и наименьшим числами.
 
-# high_and_low("1 2 3 4 5")  # return "5 1"
-# high_and_low("1 2 -3 4 5") # return "5 -3"
-# high_and_low("1 9 3 4 -5") # return
-
-
 def high_and_low(n):
-    lRes = n.split(" ")
+    lRes1 = n.split(" ")
+    lRes = [int(i) for i in lRes1]
     sRes = f"{max(lRes)} {min(lRes)}"
-    #print(lRes, sRes)
+    # print(lRes, sRes)
     return sRes
 # enddef
 
@@ -18,4 +14,6 @@ if __name__ == "__main__":
     assert high_and_low("1 2 3 4 5") == "5 1"
     assert high_and_low("1 2 -3 4 5") == "5 -3"
     assert high_and_low("1 9 3 4 -5") == "9 -5"
+    assert high_and_low('8 3 -5 42 -1 0 0 -9 4 7 4 -4') == "42 -9"
+    assert high_and_low('88 3 -5 426 -1 0 0 -956 4 7 4 -4') == "426 -956"
 # endif
