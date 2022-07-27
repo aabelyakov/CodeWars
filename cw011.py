2b3e0e9820adcd2381fa8c745c19175d58f6a72e
aabelyakov <aabelyakov@mail.ru>
2022-07-27 23:21:22 +0300

    q

--- cw011.py
+++ cw011.py
@@ -3,25 +3,31 @@
 # следует заменить отсутствующий второй символ последней пары на символ
 # подчеркивания ('_').
 
+# def solution(s):
+#     n = 0
+#     t = []
+#     while True:
+#         t.append(
+#         n += 2
 
-def solution(s):
-    n = 0
-    t = []
-    while True:
-        t.append(s[n:n + 2])
-        n += 2
-
-    return s
-
-
+    # return s
 # enddef
 
+def solution(s):
+    lOut = [s[i:i+2] for i in range(0, len(s), 2) if s]
+    iLen = len(lOut)
+    if len(lOut[iLen-1]) < 2:
+        lOut[iLen-1] += "_"
+     # endif
+    print(lOut)
+    return lOut
+# enddef
 
 if __name__ == "__main__":
+    assert solution("") == []
+    assert solution("x") == ["x_"]
     assert solution('abc') == ['ab', 'c_']
-    # assert solution('abcdef') == ['ab', 'cd', 'ef']
-    # assert solution("asdfadsf") == ['as', 'df', 'ad', 'sf']
-    # assert solution("asdfads") == ['as', 'df', 'ad', 's_']
-    # assert solution("") == []
-    # assert solution("x") == ["x_"]
+    assert solution('abcdef') == ['ab', 'cd', 'ef']
+    assert solution("asdfadsf") == ['as', 'df', 'ad', 'sf']
+    assert solution("asdfads") == ['as', 'df', 'ad', 's_']
 # endif
