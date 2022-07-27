0d2026b3bc489b6e74c06d4e0bc20219c7e9e70b
aabelyakov <aabelyakov@mail.ru>
2022-07-27 19:31:37 +0300

    q

---
+++ cw011.py
@@ -0,0 +1,21 @@
+# Завершите решение так, чтобы оно разбило строку на пары по два
+# символа. Если строка содержит нечетное количество символов, то
+# следует заменить отсутствующий второй символ последней пары на символ
+# подчеркивания ('_').
+
+
+def solution(s):
+    if
+    pass
+# enddef
+
+
+if __name__ == "__main__":
+    assert solution('abc') == ['ab', 'c_']
+    assert solution('abcdef') == ['ab', 'cd', 'ef']
+    assert solution("asdfadsf") == ['as', 'df', 'ad', 'sf']
+    assert solution("asdfads") == ['as', 'df', 'ad', 's_']
+    assert solution("") == []
+    assert solution("x") == ["x_"]
+# endif
+
