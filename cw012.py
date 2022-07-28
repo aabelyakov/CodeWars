3f50ade77a3b5f2c63c9edfa42ba63996a9a14ca
aabelyakov <aabelyakov@mail.ru>
2022-07-28 10:38:44 +0300

    cw012.py

--- cw012.py
+++ cw012.py
@@ -1,16 +1,25 @@
-# Given an array of integers as strings and numbers, return the sum of the
-# array values as if all were numbers.
-# Return your answer as a number.
 # Имея массив целых чисел в виде строк и чисел, вернуть сумму
-# значения массива, как если бы все они были числами.
+# значений массива, как если бы все они были числами.
 # Верните ответ в виде числа.
 
 def sum_mix(arr):
+    lArr = [int(i) for i in arr]
+    iSum = sum(lArr)
+    # print(lArr, iSum)
+    return iSum
+# enddef
 
-
+def sum_mix(arr):
+    return sum(map(int, arr))
 # enddef
 
 
 if __name__ == "__main__":
-
+    assert sum_mix([9, 3, '7', '3']) == 22
+    assert sum_mix(['5', '0', 9, 3, 2, 1, '9', 6, 7]) == 42
+    assert sum_mix(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']) == 41
+    assert sum_mix(['1', '5', '8', 8, 9, 9, 2, '3']) == 45
+    assert sum_mix([8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7]) == 61
 # endif
+
+
