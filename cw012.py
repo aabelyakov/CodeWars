a4ce75356b17d4f72cd45fa64537a8fe1836dd94
aabelyakov <aabelyakov@mail.ru>
2022-07-28 10:22:52 +0300

    q

---
+++ cw012.py
@@ -0,0 +1,9 @@
+# Given an array of integers as strings and numbers, return the sum of the
+# array values as if all were numbers.
+# Return your answer as a number.
+# Имея массив целых чисел в виде строк и чисел, вернуть сумму
+# значения массива, как если бы все они были числами.
+# Верните ответ в виде числа.
+
+def sum_mix(arr):
+    #your code here
