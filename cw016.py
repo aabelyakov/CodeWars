d90616ce1665e36e2d3d68616ad72b1250cec490
aabelyakov <aabelyakov@mail.ru>
2022-07-30 22:46:08 +0300

    cw016.py

--- cw016.py
+++ cw016.py
@@ -7,16 +7,16 @@
 # Не забудьте пробел после закрывающей скобки!
 
 def create_phone_number():
-    if __name__ == "__main__":
+
+# enddef
 
 
-test.assert_equals(([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]),
-                   "(123) 456-7890")
-test.assert_equals(create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
-                   "(111) 111-1111")
-test.assert_equals(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]),
-                   "(123) 456-7890")
-test.assert_equals(create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0]),
-                   "(023) 056-0890")
-test.assert_equals(create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-                   "(000) 000-0000")
+if __name__ == "__main__":
+    assert create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) == \
+        "(111) 111-1111"
+    assert create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == \
+        "(123) 456-7890"
+    assert create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0]) == \
+        "(023) 056-0890"
+    assert create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) == \
+        "(000) 000-0000"
