b0d809420121532fce79541323ac8d3e8fbed666
aabelyakov <aabelyakov@mail.ru>
2022-07-30 16:49:12 +0300

    cw014.py

---
+++ cw015.py
@@ -0,0 +1,26 @@
+# Переместите первую букву каждого слова в конец, а затем добавьте «ау» к
+# конец слова. Оставьте знаки препинания нетронутыми.
+
+
+def pig_it(text):
+
+
+
+
+# enddef
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    assert pig_it('Hello world !') == "elloHay orldway !"
+    assert pig_it('Pig latin is cool') == 'igPay atinlay siay oolcay'
+    assert pig_it('This is my string') == 'hisTay siay ymay tringsay'
+# endif
