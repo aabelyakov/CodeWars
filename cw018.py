06e1aa1bd6cabe8f8b861226b7908c2aefeb3c36
aabelyakov <aabelyakov@mail.ru>
2022-07-31 14:02:37 +0300

    cw018.py

--- cw018.py
+++ cw018.py
@@ -28,61 +28,36 @@
 
 def longest_consec(strarr, k):
     pass
+
+
 # enddef
 
 if __name__ == "__main__":
-    assert
-        longest_consec(
-            ["zone", "abigail", "theta", "form", "libe", "zas"], 2
-        ) == "abigailtheta"
     assert longest_consec(
-        ["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb",
-         "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh"
-    assert longest_consec([], 3), ""
+        ["zone", "abigail", "theta", "form", "libe", "zas"], 2
+    ) == "abigailtheta"
     assert longest_consec(
-        ["itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv",
-         "vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2),
-            "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck"
+    ["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb",
+     "oocccffuucccjjjkkkjyyyeehh"], 1
+    ) == "oocccffuucccjjjkkkjyyyeehh"
+    assert longest_consec([], 3) == ""
     assert longest_consec(
-        ["wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu"], 2),
-            "wlwsasphmxxowiaxujylentrklctozmymu"
-    assert
-        longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"],
-                       -2), ""
-    assert
-        longest_consec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 3),
-        "ixoyx3452zzzzzzzzzzzz"
-    assert
-        longest_consec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 15),
-        ""
-    assert
-        longest_consec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 0),
-        ""
+    ["itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv",
+     "vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2
+    ) == "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck"
+    assert longest_consec(
+    ["wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu"], 2
+    ) == "wlwsasphmxxowiaxujylentrklctozmymu"
+    assert longest_consec(
+        ["zone", "abigail", "theta", "form", "libe","zas"], -2
+    ) == ""
+    assert longest_consec(
+        ["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 3
+    ) == "ixoyx3452zzzzzzzzzzzz"
+    assert longest_consec(
+        ["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 15
+    ) == ""
+    assert longest_consec(
+        ["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 0
+    ) == ""
 # endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
