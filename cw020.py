2e1abc8b00deed246e46888034eca56436c837a1
aabelyakov <aabelyakov@mail.ru>
2022-08-02 11:41:14 +0300

    cw020.py

--- cw020.py
+++ cw020.py
@@ -12,26 +12,19 @@
 def first_non_repeating_letter(s):
 
 
-
 # enddef
 
 
 if __name__ == "__main__":
-test.assert_equals(first_non_repeating_letter('a'), 'a')
-test.assert_equals(first_non_repeating_letter('stress'), 't')
-test.assert_equals(first_non_repeating_letter('moonmen'), 'e')
-
-test.it('should handle empty strings')
-test.assert_equals(first_non_repeating_letter(''), '')
-
-test.it('should handle all repeating strings')
-test.assert_equals(first_non_repeating_letter('abba'), '')
-test.assert_equals(first_non_repeating_letter('aa'), '')
-
-test.it('should handle odd characters')
-test.assert_equals(first_non_repeating_letter('~><#~><'), '#')
-test.assert_equals(first_non_repeating_letter('hello world, eh?'), 'w')
-
-test.it('should handle letter cases')
-test.assert_equals(first_non_repeating_letter('sTreSS'), 'T')
-test.assert_equals(first_non_repeating_letter('Go hang a salami, I\'m a lasagna hog!'), ',')
+    assert first_non_repeating_letter('a') == 'a'
+    assert first_non_repeating_letter('stress') == 't'
+    assert first_non_repeating_letter('moonmen') == 'e'
+    assert first_non_repeating_letter('') == ''
+    assert first_non_repeating_letter('abba') == ''
+    assert first_non_repeating_letter('aa') == ''
+    assert first_non_repeating_letter('~><#~><') == '#'
+    assert first_non_repeating_letter('hello world, eh?') == 'w'
+    assert first_non_repeating_letter('sTreSS') == 'T'
+    assert first_non_repeating_letter(
+        'Go hang a salami, I\'m a lasagna hog!') == ','
+# endif                  
