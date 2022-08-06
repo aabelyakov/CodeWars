55785f98e9855c145d8c0ac61bf6bf465273ce3d
aabelyakov <aabelyakov@mail.ru>
2022-08-06 20:53:27 +0300

    cw022.py

--- cw022.py
+++ cw022.py
@@ -1,17 +1,9 @@
-# Если строка уже заканчивается числом  == число должно быть
+# Если строка уже заканчивается числом, то число должно быть
 # увеличено на 1.
-# Если строка не заканчивается цифрой  == то цифра 1 должна быть добавлена
+# Если строка не заканчивается цифрой, то цифра 1 должна быть добавлена
 # к новой строке.
-#
-# Внимание: Если в числе есть ведущие нули  == количество цифр должно быть
+# Внимание: Если в числе есть ведущие нули, то количество цифр должно быть
 # сохранено.
-#
-# Примеры:
-# foo -> foo1
-# foobar23 -> foobar24
-# foo0042 -> foo0043
-# foo9 -> foo10
-# foo099 -> foo100
 
 
 def increment_string(s: str) -> str:
@@ -45,6 +37,9 @@ if __name__ == "__main__":
     assert increment_string("foo") == "foo1"
     assert increment_string("foobar001") == "foobar002"
     assert increment_string("foobar1") == "foobar2"
+    assert increment_string("foobar23") == "foobar24"
+    assert increment_string("foo0042") == "foo0043"
+    assert increment_string("foo09") == "foo10"
     assert increment_string("foobar00") == "foobar01"
     assert increment_string("foobar99") == "foobar100"
     assert increment_string("foobar099") == "foobar100"
