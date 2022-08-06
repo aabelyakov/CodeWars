e996af03dc16a53bcc80d174d8c5059499b0d65f
aabelyakov <aabelyakov@mail.ru>
2022-08-06 20:47:12 +0300

    cw022.py

--- cw022.py
+++ cw022.py
@@ -16,22 +16,20 @@
 
 def increment_string(s: str) -> str:
     if s:
-        s1 = ""
+        s1 = s2 = ""
         for i, k in enumerate(s):
             if k.isalpha():
                 s1 += k
-                continue
             elif k.isdigit():
-                break
+                s2 += k
             # endif
         # endfor
-        s2 = s[i:]
-        print(s2)
+        # print(s2)
         if s2:
             iL2 = len(s2)
             s3 = str(int(s2) + 1)
             s4 = s3.rjust(iL2, "0")
-            print(s1 + s4)
+            # print(s1 + s4)
             return s1 + s4
         else:
             return s1 + "1"
@@ -39,21 +37,16 @@ def increment_string(s: str) -> str:
     else:
         s1 = "1"
         return s1
-
-    # {s3:0}
-
-
-# print(f'{10:0{max_width}}')      # 00010   - вывод с добавлением нолей
-# 'foo'.rjust(7, '+') == '++++foo'
+    # endif
 # enddef
 
 
 if __name__ == "__main__":
     assert increment_string("foo") == "foo1"
     assert increment_string("foobar001") == "foobar002"
-    # assert increment_string("foobar1") == "foobar2"
-    # assert increment_string("foobar00") == "foobar01"
-    # assert increment_string("foobar99") == "foobar100"
-    # assert increment_string("foobar099") == "foobar100"
-    # assert increment_string("") == "1"
+    assert increment_string("foobar1") == "foobar2"
+    assert increment_string("foobar00") == "foobar01"
+    assert increment_string("foobar99") == "foobar100"
+    assert increment_string("foobar099") == "foobar100"
+    assert increment_string("") == "1"
 # endif
