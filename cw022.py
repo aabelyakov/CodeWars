00b8d0e5716be3ad04db66fbd925da5fcb19e44b
aabelyakov <aabelyakov@mail.ru>
2022-08-07 08:42:23 +0300

    cw022.py

--- cw022.py
+++ cw022.py
@@ -9,26 +9,30 @@
 def increment_string(s: str) -> str:
     if s:
         s1 = s2 = ""
-        for i, k in enumerate(s):
+        res = "".join(reversed(s))
+        for i, k in enumerate(res):
             if k.isdigit():
-                s2 += k
-            else:
                 s1 += k
+            else:
+                s2 += k
             # endif
         # endfor
-        print(s2)
-        if s2:
-            iL2 = len(s2)
-            s3 = str(int(s2) + 1)
+        s21 = "".join(reversed(s1))
+        s11 = "".join(reversed(s2))
+        # print('s11 =', s11)
+        # print("s21 =", s21)
+        if s21:
+            iL2 = len(s21)
+            s3 = str(int(s21) + 1)
             s4 = s3.rjust(iL2, "0")
-            print(s1 + s4)
-            return s1 + s4
+            # print(s11 + s4)
+            return s11 + s4
         else:
-            return s1 + "1"
+            return s11 + "1"
         # endif
     else:
-        s1 = "1"
-        return s1
+        s11 = "1"
+        return s11
     # endif
 # enddef
 
@@ -45,4 +49,6 @@ if __name__ == "__main__":
     assert increment_string("foobar099") == "foobar100"
     assert increment_string("") == "1"
     assert increment_string("[k'M 0208970000067") == "[k'M 0208970000068"
+    assert increment_string(
+        '2878@4Tc7Aq3n033KxxjsO9o8316825537:T*Pm225852670000') == '2878@4Tc7Aq3n033KxxjsO9o8316825537:T*Pm225852670001'
 # endif
