c29a803c4c4a7749275a88cb8bc92512101917a1
aabelyakov <aabelyakov@mail.ru>
2022-08-11 11:16:42 +0300

    cw025.py

--- cw025.py
+++ cw025.py
@@ -6,4 +6,9 @@ print(num2words(42, to='ordinal'))
 print(num2words(42, lang='fr'))
 # quarante-deux
 print(num2words(42, lang='ru'))
-print(num2words(9875487542, lang='ru'))
+# сорок два
+print(num2words(9875487542.87, lang='ru'))
+# девять миллиардов восемьсот семьдесят пять миллионов четыреста восемьдесят
+# семь тысяч пятьсот сорок два
+print(num2words(42.346, lang='ru'))
+# сорок два запятая триста сорок шесть
