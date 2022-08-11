093929f82bf8f1a8801fb672a0c254cfd4253ce5
aabelyakov <aabelyakov@mail.ru>
2022-08-11 11:10:41 +0300

    requirements.txt

--- cw024.py
+++ cw024.py
@@ -5,29 +5,17 @@
 # Максимальное число - 1 миллион, в т.ч. "сто двадцать четыре" не
 # обязательно, и в одних случаях присутствует, а в других - нет.
 
-from word2number import w2n
-
-slova = [
-    'ноль', 'один', 'два', 'три', 'четыре', 'пять',
-    'шесть', 'семь', 'восемь', 'девять', 'десять'
-]
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-
-slovar = dict(zip(numbers, slova))
-print(slovar)
-print(slovar.get('0'))
-# 'ноль'
-slovar.update(zip(slova, numbers))
-print(slovar)
-print(slovar.get('ноль'))
-# '0'
 
 
 if __name__ == "__main__":
+    # Загрузить word2number через pip
+    from word2number import w2n
+
     assert w2n.word_to_num(
         "two million three thousand nine hundred and eighty four"
     ) == 2003984
     assert w2n.word_to_num("twenty") == 20
+    print(w2n.word_to_num("twenty"))
     assert w2n.word_to_num("two hundred forty-six") == 246
     assert w2n.word_to_num(
         "seven hundred eighty-three thousand nine hundred and nineteen"
