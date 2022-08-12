d3838801387a00f765f468cc147c6f6e02b23fa5
aabelyakov <aabelyakov@mail.ru>
2022-08-12 09:02:02 +0300

    cw026.py

--- cw026.py
+++ cw026.py
@@ -64,24 +64,26 @@ def order_weight(s):
     # endfor
     return sRes.rstrip()
 # enddef
-# dict(sorted(rooms.items()))
-# sorted(rooms.items())
+
+
+# Потрясающие решения
+# def order_weight(s):
+#     return ' '.join(sorted(sorted(s.split(' ')), key=lambda x: sum(int(c) for c in x)))
+# # enddef
+
+
+# def order_weight(s):
+#     return ' '.join(sorted(s.split(), key=lambda x: (sum(map(int, x)), x)))
+# # enddef
+
+
 if __name__ == "__main__":
-    # print(rec(103))
-    # print(rec(123))
-    # print(rec(444))
-    # print(rec(99))
-    # print(rec(2000))
-    # assert order_weight("103 123 4444 99 2000") == "2000 103 123 4444 99"
-    # print(order_weight("56 65 74 100 99 68 86 180 90"))
-    # assert order_weight(
-    #     "56 65 74 100 99 68 86 180 90"
-    # ) == "100 180 90 56 65 74 68 86 99"
-    # print(order_weight(" 103  123 4444    99 2000  "))
-    # assert order_weight(
-    #     "2000 10003 1234000 44444444 9999 11 11 22 123"
-    # ) == "11 11 2000 10003 22 123 1234000 44444444 9999"
-    print(order_weight(
+    assert order_weight("103 123 4444 99 2000") == "2000 103 123 4444 99"
+    assert order_weight(
+        "56 65 74 100 99 68 86 180 90"
+    ) == "100 180 90 56 65 74 68 86 99"
+    assert order_weight(
         "2000 10003 1234000 44444444 9999 11 11 22 123"
-    ))
+    ) == "11 11 2000 10003 22 123 1234000 44444444 9999"
     assert order_weight("") == ""
+# endif
