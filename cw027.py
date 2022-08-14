6298cf73b89fe58c7cf11339a1d547c58ddf17d4
aabelyakov <aabelyakov@mail.ru>
2022-08-14 17:07:52 +0300

    q

--- cw027.py
+++ cw027.py
@@ -18,36 +18,38 @@ from intvalpy import Interval
 # В случае, когда пользователь хочет создать массив интервалов, то ему
 # следует указать массивы правых и левых концов. Например:
 
-a = [2, 7, -3]
-b = [4, 5, 1]
-data = Interval(a, b)
-print(data)
+# a = [2, 7, -3]
+# b = [4, 5, 1]
+# data = Interval(a, b)
+# print(data)
 
 def solution(s):
+    # Предшествующий элемент списка
     m = s[0]
-    lRes = [m]
-    lRes1 = []
+    lRes = []
+    # lRes1 = []
     n = 1
     for k in s[1:]:
-        if k - m != 1:
+        if k - m == 1:
+            lRes.append(m)
             lRes.append(str(k))
-        else:
-            # Интервал
-            lRes1.append(str(k))
+        # else:
+        #     # Интервал
+        #     lRes1.append(str(k))
         m = k
         n += 1
     # endfor
-    print(lRes)
-    print(lRes1)
-    for c in lRes1:
-        pass
-    return
+    # print()
+    # print(lRes1)
+    # for c in lRes1:
+    #     pass
+    return lRes
 # enddef
 
 
 if __name__ == "__main__":
     pass
-    # print(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8]))
+    print(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8]))
     # assert solution([
     #     -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8,
     #     9, 10, 11, 14, 15, 17, 18, 19, 20
