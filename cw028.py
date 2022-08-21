5d4bed7151ee23f4578d2c4aec5a226e6cd2ec8c
aabelyakov <aabelyakov@mail.ru>
2022-08-21 21:22:42 +0300

    q

--- cw028.py
+++ cw028.py
@@ -3,26 +3,23 @@
 # В этом ката вы должны создать все перестановки непустой входной строки и
 # удалить дубликаты, если они есть. Это означает, что вы должны перетасовать
 # все буквы из ввода во всех возможных порядках.
-#
-# Примеры:
-#
-# * С вводом «а»
-# * Ваша функция должна вернуть: ['a']
-#
-# * С вводом «ab»
-# * Ваша функция должна возвращать ['ab', 'ba']
-#
-# * С вводом 'aabb'
-# * Ваша функция должна возвращать ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
-# Порядок перестановок не имеет значения.
+
+
+from itertools import permutations
 
 
 def permutations(s):
-    pass
+    # lst = [''.join(p) for p in permutations(s)]
+    lst = list(permutations(s))
+    print(lst)
 # enddef
 
 
 if __name__ == "__main__":
-    assert sorted(permutations('a')) == ['a']
-    assert sorted(permutations('ab')), ['ab', 'ba'])
-    assert sorted(permutations('aabb')), ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'])
+    # assert sorted(permutations('a')) == ['a']
+    # assert sorted(permutations('ab')) == ['ab', 'ba']
+    print(sorted(permutations('ab')))
+    # assert sorted(permutations('aabb')) == [
+    #     'aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'
+    # ]
+# endif
