9a3496db8a6e0a7c3752c5011b0586c60ee0c972
aabelyakov <aabelyakov@mail.ru>
2022-09-06 13:23:36 +0300

    q

--- cw024.py
+++ cw024.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         "two million three thousand nine hundred and eighty four"
     ) == 2003984
     assert w2n.word_to_num("twenty") == 20
-    print(w2n.word_to_num("twenty"))
+    # print(w2n.word_to_num("twenty"))
     assert w2n.word_to_num("two hundred forty-six") == 246
     assert w2n.word_to_num(
         "seven hundred eighty-three thousand nine hundred and nineteen"
